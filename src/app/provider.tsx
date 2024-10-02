38f644feb40f9a6eafeace510c53cddafdeca4ae
Bahadur_mia <96464210+Bahadur777@users.noreply.github.com>
2024-10-02 21:37:23 +0600

    clerk setup

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import {NextUIProvider} from "@nextui-org/system";
+import { ClerkProvider } from '@clerk/nextjs';
 const provider = ({children}:{children:React.ReactNode}) => {
     
   return (
+    <ClerkProvider>
     <NextUIProvider>
         {children}
     </NextUIProvider>
+    </ClerkProvider>
   )
 }
 
